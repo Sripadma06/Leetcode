41ae294183018fdda6dba1999060a4fa0cf1f489
Sripadma Kulkarni <127661259+Sripadma06@users.noreply.github.com>
2026-09-07 20:16:55 +0530

    Time: 0 ms (100%), Space: 48.1 MB (71.55%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,70 +1,55 @@
+
 class Solution {
-
     public int[] searchRange(int[] nums, int target) {
+        int first = this.findBound(nums, target, true);
 
-        int first = findFirst(nums, target);
-        int last = findLast(nums, target);
-
-        return new int[]{first, last};
-    }
-
-    private int findFirst(int[] nums, int target) {
-
-        int left = 0;
-        int right = nums.length - 1;
-        int result = -1;
-
-        while (left <= right) {
-
-            int mid = left + (right - left) / 2;
-
-            if (nums[mid] == target) {
-
-                result = mid;
-
-                // Search for an earlier occurrence
-                right = mid - 1;
-
-            } else if (nums[mid] < target) {
-
-                left = mid + 1;
-
-            } else {
-
-                right = mid - 1;
-            }
+        if(first == -1){
+            return new int[]{-1,-1};
         }
 
-        return result;
+        int last = this.findBound(nums, target, false);
+        
+        return new int[]{first,last};
     }
 
-    private int findLast(int[] nums, int target) {
+    public int findBound(int[] nums, int target, boolean isFirst){
 
-        int left = 0;
-        int right = nums.length - 1;
-        int result = -1;
+        int start = 0;
+        int end = nums.length - 1;
 
-        while (left <= right) {
+        while(start<= end){
+            int mid = (start+end)/2;
 
-            int mid = left + (right - left) / 2;
+            if(nums[mid] == target ){
+                if(isFirst){
 
-            if (nums[mid] == target) {
+                if(mid == start || nums[mid-1]!= target){
+                    return mid;
+                }
 
-                result = mid;
+                end = mid-1;
 
-                // Search for a later occurrence
-                left = mid + 1;
-
-            } else if (nums[mid] < target) {
-
-                left = mid + 1;
-
-            } else {
-
-                right = mid - 1;
             }
-        }
 
-        return result;
+            else{
+                if(mid == end || nums[mid+1] != target){
+                    return mid;
+                }
+                start = mid+1;
+            }
+
+
+            }
+            
+            else if(nums[mid] > target){
+                end = mid-1;
+            }
+            else{
+                start = mid+1;
+            }
+
+        }
+        return -1;
+
     }
 }
