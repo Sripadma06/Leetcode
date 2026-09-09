678b3d8ce851441da061925cb5caa15689316190
Sripadma Kulkarni <127661259+Sripadma06@users.noreply.github.com>
2026-09-09 21:04:24 +0530

    Time: 13 ms (28.4%), Space: 46.8 MB (9.09%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        Map<Character, Integer> count = new HashMap<>();
+
+        for (char c : s.toCharArray()) {
+            count.put(c, count.getOrDefault(c, 0) + 1);
+        }
+
+        for (char c : t.toCharArray()) {
+            count.put(c, count.getOrDefault(c, 0) - 1);
+        }
+
+        for (int frequency : count.values()) {
+            if (frequency != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
